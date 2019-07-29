ed3de79a2787a9a0997c90898dd023cf609d3958
Manoj kushwah <design@jsinformatics.com>
2019-07-29 18:01:10 +0530

    Command excute by '.exe' file.

---
+++ c++/jdesk9.cpp
@@ -0,0 +1,18 @@
+// A C++ program that compiles and runs another C++  
+// program 
+#include <bits/stdc++.h> 
+using namespace std; 
+int main (int arg1, char *fileName[]) 
+{  
+//	cout << "\n\narg1: ";
+//	cout << arg1;
+//	cout << "\n\nfileName: ";
+//	cout << *fileName;
+//	cout << "\n\nRun app: ";
+//	system("c:\\jdesk9\\jdesk.bat"); 
+//	cout << "\n\n";
+
+	system("C:\\jdesk9\\bin\\java -noverify -jar C:\\jdesk9\\bin\\JtepSpring.jar"); 
+	cout << "\n\n";
+    return 0; 
+} 
